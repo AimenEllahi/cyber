215c7aad87ab8123acc19370d4e102a43807d0f7
Aimen Ellahi <you@example.com>
2023-08-09 21:05:59 +0500

    added model

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
-import { useEffect } from 'react';
+import { Suspense, useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
+import { useLoader, Canvas, useFrame } from '@react-three/fiber';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import {useControls} from 'leva';
 import LocomotiveScroll from 'locomotive-scroll';
 import './App.css';
+import * as THREE from 'three';
 
 gsap.registerPlugin(ScrollTrigger);
 
+const Model = () => {
+  const fbxRef = useRef();
+  const fbx = useLoader(FBXLoader, '/StandingArguing.fbx');
+  const clips = fbx.animations;
+  const mixer = useRef();
+
+  useEffect(() => {
+    mixer.current = new THREE.AnimationMixer(fbxRef.current);
+
+    if (clips && clips.length > 0) {
+      const animationAction = mixer.current.clipAction(clips[0]);
+      animationAction.play();
+    }
+
+    return () => {
+      // Clean up the mixer when the component is unmounted
+      mixer.current.stopAllAction();
+    };
+  }, [clips]);
+
+  useFrame((state, delta) => {
+    if (mixer.current) {
+      mixer.current.update(delta);
+    }
+  });
+
+  const { scale, positions, rotations } = useControls('controls', {
+    scale: { value: 0.05, min: 0.01, max: 0.1 },
+    positions: { value: [0.4,-7.9,1.3], step: 0.1 },
+    rotations: { value: [0, 0, 0], step: 0.1 },
+  });
+
+  return (
+    <group ref={fbxRef} scale={scale} position={positions} rotation={rotations}>
+      <primitive object={fbx} />
+    </group>
+  );
+};
 function App() {
   useEffect(() => {
     const locoScroll = new LocomotiveScroll({
@@ -63,6 +105,15 @@ function App() {
 
   return (
     <div>
+        <div className='fixed w-screen h-screen top-0 left-0 z-[30] pointer-events-none'>
+          <Canvas style={{ border: "1px solid black" }}>
+          <ambientLight intensity={0.2} />
+          <spotLight position={[10, 15, 10]} angle={0.3} />
+          <Suspense fallback={null}>
+          <Model />
+          </Suspense>
+          </Canvas>
+        </div>
      <div id="nav" className="flex items-center justify-between h-7 w-full fixed z-50 px-4">
         <h3 className="font-gilroy font-normal text-22">CYBERFICTION*</h3>
         <button className="px-10 py-2 rounded-full bg-black text-white border-none">APRIL, 2023</button>
@@ -76,7 +127,6 @@ function App() {
             CYBERFICTION AIMS TO BE A DECENTRALIZED COMMUNITY THAT CAN <br /> CREATE NEW VALUES AND PROFITS THROUGH PLAY IN THE VIRTUAL <br /> WORLD.
           </h3>
           <h4 className="absolute top-62 font-gilroy font-semibold left-25">...SCROLL TO READ</h4>
-          <canvas></canvas>
         </div>
         <div id="page1" className="relative h-screen w-screen bg-gray-200">
           <div id="right-text" className="absolute top-30 left-10">
